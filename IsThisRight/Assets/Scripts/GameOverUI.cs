685f41d38c502e860878449ecfc0ea592ab5644c
Sebastian Segura <sebassegurasoto@gmail.com>
2020-12-16 21:32:19 -0600

    Level 2 Almost Done

---
+++ IsThisRight/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    public void Retry()
+    {
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:0);
+    }
+}
---
+++ IsThisRight/Assets/Scripts/TrampaBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampaBehavior : MonoBehaviour
+{
+    private Animator trampaClose;
+    public AudioSource trapSound;
+    void Start()
+    {
+        trampaClose = GetComponent<Animator>();
+    }
+    void OnCollisionEnter2D()
+    {
+        trapSound.Play();
+        trampaClose.Play("trampa_cerrandose");
+    }
+    void OnCollisionExit2D()
+    {
+        trampaClose.Play("trampa_abriendose");
+    }
+}
